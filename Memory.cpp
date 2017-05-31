112728270a822a467f30b10528af3f156f2567d0
Philip Jacobs <jacobs.philip@aikq.de>
2017-05-31 23:58:17 +0200

    Initial Commit [2]

---
+++ Memory.cpp
@@ -0,0 +1,156 @@
+#include <string>
+#include <iostream>
+#include <unistd.h>
+#include <ApplicationServices/ApplicationServices.h>
+#include <thread>
+#include <map>
+#include <stdio.h>
+#include <stdlib.h>
+#include <mach/mach_traps.h>
+#include <mach/mach_init.h>
+#include <mach/mach_error.h>
+#include <mach/mach.h>
+#include <mach-o/dyld_images.h>
+#include <mach-o/loader.h>
+#include <libproc.h>
+#include <sys/stat.h>
+
+
+using namespace std;
+
+pid_t   mainPid     = -1;
+task_t  mainTask    = 0;
+
+class Process
+{
+public:
+    int get(const char * procname)
+    {
+        pid_t pids[1024];
+        int numberOfProcesses = proc_listpids(PROC_ALL_PIDS, 0, NULL, 0);
+        proc_listpids(PROC_ALL_PIDS, 0, pids, sizeof(pids));
+        for (int i = 0; i < numberOfProcesses; ++i)
+        {
+            if (pids[i] == 0) { continue; }
+            char name[1024];
+            proc_name(pids[i], name, sizeof(name));
+            if (!strncmp(name, (const char *)procname, sizeof(procname)))
+            {
+                return pids[i];
+            }
+        }
+        return -1;
+    }
+    
+    int task(pid_t pid)
+    {
+        task_t task = 0;
+        task_for_pid(current_task(), pid, &task);
+        
+        return task;
+    }
+    
+    void getModule(task_t task, mach_vm_address_t * start, off_t * length, const char * module, Byte * buffer = nullptr, bool readBuffer = false)
+    {
+        struct task_dyld_info dyld_info;
+        mach_vm_address_t address = 0;
+        mach_msg_type_number_t count = TASK_DYLD_INFO_COUNT;
+        if (task_info(task, TASK_DYLD_INFO, (task_info_t)&dyld_info, &count) == KERN_SUCCESS)
+        {
+            address = dyld_info.all_image_info_addr;
+        }
+        
+        struct dyld_all_image_infos *dyldaii;
+        mach_msg_type_number_t size = sizeof(dyld_all_image_infos);
+        vm_offset_t readMem;
+        kern_return_t kr = vm_read(task, address, size, &readMem, &size);
+        if (kr != KERN_SUCCESS)
+        {
+            return;
+        }
+        
+        dyldaii = (dyld_all_image_infos *) readMem;
+        int imageCount = dyldaii->infoArrayCount;
+        mach_msg_type_number_t dataCnt = imageCount * 24;
+        struct dyld_image_info *g_dii = NULL;
+        g_dii = (struct dyld_image_info *) malloc (dataCnt);
+        // 32bit bs 64bit
+        kern_return_t kr2 = vm_read(task, (mach_vm_address_t)dyldaii->infoArray, dataCnt, &readMem, &dataCnt);
+        if (kr2)
+        {
+            return;
+        }
+        struct dyld_image_info *dii = (struct dyld_image_info *) readMem;
+        for (int i = 0; i < imageCount; i++)
+        {
+            dataCnt = 1024;
+            vm_read(task, (mach_vm_address_t)dii[i].imageFilePath, dataCnt, &readMem, &dataCnt);
+            char *imageName = (char *)readMem;
+            if (imageName)
+            {
+                g_dii[i].imageFilePath = strdup(imageName);
+            }
+            else
+            {
+                g_dii[i].imageFilePath = NULL;
+            }
+            g_dii[i].imageLoadAddress = dii[i].imageLoadAddress;
+            if (strstr(imageName, module) != NULL)
+            {
+                struct stat st;
+                stat(imageName, &st);
+                *start = (mach_vm_address_t)dii[i].imageLoadAddress;
+                
+                *length = st.st_size;
+            }
+        }
+    }
+};
+
+Process * g_cProc = new Process();
+
+class MemMngr
+{
+    
+public:
+    template <typename type>
+    type read(mach_vm_address_t address, size_t extraSize = sizeof(type))
+    {
+        type content;
+        
+        vm_offset_t data;
+        uint32_t size;
+        auto callback = vm_read(mainTask, (vm_address_t)address, extraSize, &data, &size);
+        if(callback == KERN_SUCCESS)
+        {
+            content = (type) *(type *)(data);
+            vm_deallocate(current_task(), data, size);
+        }
+        
+        return content;
+    }
+
+    
+    template <typename type>
+    bool write(mach_vm_address_t address, type data)
+    {
+        if(address == 0)
+        {
+            return false;
+        }
+        
+        auto callback = vm_write(mainTask, address, (vm_offset_t)&data, sizeof(data));
+        if (callback == KERN_SUCCESS)
+        {
+            return true;
+        }
+        
+        return false;
+    }
+    
+    
+    
+};
+
+MemMngr * mem = new MemMngr();
+
